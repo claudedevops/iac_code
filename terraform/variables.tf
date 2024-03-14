a4c454c32b5da0c9ea4e60b929fa852cdd13ffb7
Claude Eke <visionary_p@yahoo.com>
2024-03-14 12:57:41 -0700

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -56,7 +56,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "gitopskey"
+  default     = "claudekey33"
 }
 
 variable "instance_name" {
